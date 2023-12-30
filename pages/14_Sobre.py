da5cddd866aec34c29d8ad9afb0a0f9b629a665a
Leonardo Melo Costa da Silva <leonardo.costa@mpba.mp.br>
2023-12-30 02:10:09 -0300

    Update 14_Sobre.py

--- pages/14_Sobre.py
+++ pages/14_Sobre.py
@@ -10,7 +10,9 @@ st.write("Leonardo Melo e Milena de Carvalho Cordeiro")
 
 st.divider()
 
-st.subheader('INSTRUÇÕES PARA EXECUTAR LOCAL')
+st.subheader('Funcionalidades do Aplicativo')
 
-st.write("""Para gerar o PDF é necessário executar o código em um servidor local do streamlint, pois o
-         componente de pdf só funciona localmente""")
+st.markdown('1. ChatBot que orienta o usuário nos casos de Violência contra a Mulher.')
+st.markdown('2. Um bot que responde perguntas baseado no pdf carregado sobre a vida do Piloto Ayrton Senna.')
+st.markdown('3 Modelo que apresenta uma previsão se o piloto pontuou ou não em uma corrida baseado nas informações fornecidas.')
+st.markdown('4. Gerador de E-mail(s) personalizados a partir de informações de um potencial cliente. Sendo que os dados desses clientes são carregados em uma lista, onde o usuário pode selecionar um ou mais clientes. Adicionalmente, o texto do e-mail gerado pode ser baixado como pdf.')
