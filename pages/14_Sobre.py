21297509a4fdce6261c20beb216a5778598432a7
Leonardo Melo Costa da Silva <leonardo.costa@mpba.mp.br>
2023-12-30 02:11:10 -0300

    Update 14_Sobre.py

--- pages/14_Sobre.py
+++ pages/14_Sobre.py
@@ -4,7 +4,7 @@ st.header('💙 SOBRE')
 
 st.divider()
 
-st.subheader('COMPONENTES DO GRUPO')
+st.subheader('Componentes do Grupo')
 
 st.write("Leonardo Melo e Milena de Carvalho Cordeiro")
 
@@ -14,5 +14,5 @@ st.subheader('Funcionalidades do Aplicativo')
 
 st.markdown('1. ChatBot que orienta o usuário nos casos de Violência contra a Mulher.')
 st.markdown('2. Um bot que responde perguntas baseado no pdf carregado sobre a vida do Piloto Ayrton Senna.')
-st.markdown('3 Modelo que apresenta uma previsão se o piloto pontuou ou não em uma corrida baseado nas informações fornecidas.')
+st.markdown('3. Modelo que apresenta uma previsão se o piloto pontuou ou não em uma corrida baseado nas informações fornecidas.')
 st.markdown('4. Gerador de E-mail(s) personalizados a partir de informações de um potencial cliente. Sendo que os dados desses clientes são carregados em uma lista, onde o usuário pode selecionar um ou mais clientes. Adicionalmente, o texto do e-mail gerado pode ser baixado como pdf.')
